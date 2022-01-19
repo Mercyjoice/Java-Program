4df290a41e910ccc920dc569fd8a29c5d9110635
JoshuaRobinsonJ <97521677+JoshuaRobinsonJ@users.noreply.github.com>
2022-01-19 21:16:28 +0530

    Add files via upload

---
+++ Assignment 5/Assignment 5/NaturalNums.java
@@ -0,0 +1,12 @@
+class NaturalNums
+{
+public static void main(String[] args)
+{     
+int i;
+System.out.println("The 10 Natural numbers are:\n");
+for(i=1;i<=10;i++)
+{      
+System.out.println (i);
+}
+}
+}
